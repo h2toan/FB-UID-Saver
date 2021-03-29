bf1f2ac97019ec24ce78cec2ef583e0aa759a21a
Sorrow <h2toan@gmail.com>
2021-03-29 17:03:54 +0700

    query element and insert button

---
+++ internal.js
@@ -0,0 +1,7 @@
+import $ from './jquery-3.6.0.slim.min.js';
+$(document).ready(function () {
+    let temp1 = $('span.d2edcug0.hpfvmrgz.qv66sw1b.c1et5uql.lr9zc1uh.a8c37x1j.keod5gw0.nxhoafnm.aigsh9s9.d9wwppkn.fe6kdd0r.mau55g9w.c8b282yb.mdeji52x.e9vueds3.j5wam9gi.knj5qynh.m9osqain.hzawbc8m');
+    for (let i = 0; i < temp1.length; i++) {
+        temp1[i].firstElementChild.innerHTML += `<img src="chrome-extension://gjbgieplgdekhcjbelpbjjfkffodekol/img/marker.svg" style="height:17px;position:relative;top:3px;left:5px;"></img>`
+    };
+});
