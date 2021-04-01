fe94dbf38d13ca7b707ed309fb5e3e2936e67c03
Sorrow <h2toan@gmail.com>
2021-04-01 17:30:57 +0700

    working on saving data to google sheet

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 chrome.runtime.onMessageExternal.addListener(
     function (request) {
-        console.log(request);
-        fetch('https://script.google.com/macros/s/AKfycbx7zafCoJwOWE34SK0RX_a3irP5mAetJVfAZz6xeJR5GCEfhB_HIW5b97MdUoEU-vxq/exec', {
+        fetch('https://script.google.com/macros/s/AKfycbxgG65tcpImwBfbFK0wpdFqqm9Jh3RxAXoB4G78K53EQAryZDjqZMESj3t-F-T6kEfB/exec', {
             method: 'POST',
             mode: 'no-cors',
             body: JSON.stringify(request)
--- main.gs.js
+++ main.gs.js
@@ -1,4 +1,4 @@
-const TEST_PAYLOAD = JSON.parse('[{"uid":"100001737604884","name":"Vịt Shmily","timeTaken":"2021-04-01T02:45:40.178Z"},{"uid":"100004947794849","name":"Lan Hương","timeTaken":"2021-04-01T02:47:17.260Z"},{"uid":"100010126620062","name":"Đỗ Thị Nguyên Phương","timeTaken":"2021-04-01T02:46:12.384Z"},{"uid":"100025576605692","name":"NY NY","timeTaken":"2021-04-01T03:03:43.848Z"},{"uid":"100034109737914","name":"Hoa Anh Đào","timeTaken":"2021-04-01T03:01:42.667Z"},{"uid":"100039588876156","name":"Linh Nhi","timeTaken":"2021-04-01T02:47:23.072Z"},{"uid":"100040985168445","name":"Tuyết Trinh","timeTaken":"2021-04-01T02:50:46.320Z"},{"uid":"100047493000935","name":"Hà Như","timeTaken":"2021-04-01T02:46:10.009Z"},{"uid":"100050702328351","name":"Vivian Vo","timeTaken":"2021-04-01T03:03:38.424Z"},{"uid":"100062700468518","name":"Hạnh Đù","timeTaken":"2021-04-01T02:46:06.704Z"}]');
+const TEST_PAYLOAD = JSON.parse('{"uid":"100047590807335","name":"Lê Thanh Ngân","timeTaken":"2021-04-01T09:02:07.770Z"}');
 
 function writeToDatabase(PAYLOAD = TEST_PAYLOAD) {
     const DATABASE = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Sheet2');
